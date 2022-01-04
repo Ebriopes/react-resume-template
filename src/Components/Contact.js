0d599512274159392c08f6599212152be1910a71
Ebriopes <antonio.vargasrosales@gmail.com>
2022-01-04 14:34:35 -0600

    features: add my info, change the styles and submit

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,20 +1,30 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 
-class Contact extends Component {
-  render() {
+const Contact = ({ data }) => {
+   const [url, setUrl] = useState();
+   const [name, setName] = useState('');
+   const [subject, setSubject] = useState('');
+   const [email, setEmail] = useState('');
+   const [message, setMessage] = useState('');
 
-    if(this.props.data){
-      var name = this.props.data.name;
-      var street = this.props.data.address.street;
-      var city = this.props.data.address.city;
-      var state = this.props.data.address.state;
-      var zip = this.props.data.address.zip;
-      var phone= this.props.data.phone;
-      var email = this.props.data.email;
-      var message = this.props.data.contactmessage;
-    }
+   const handleClick = (e) => {
+      e.preventDefault();
 
-    return (
+      window.open(url);
+   }
+
+   const handleChange = () => {
+      setUrl(`mailto:${email}?subject=${name}: ${subject}&body=${message}`)
+   }
+
+   useEffect(() => {
+      if (data) {
+         setEmail(data.email);
+         setUrl(`mailto:${data.email}`)
+      }
+   }, [data])
+
+   return (
       <section id="contact">
 
          <div className="row section-head">
@@ -27,7 +37,7 @@ class Contact extends Component {
 
             <div className="ten columns">
 
-                  <p className="lead">{message}</p>
+               <p className="lead">{data?.message}</p>
 
             </div>
 
@@ -36,83 +46,59 @@ class Contact extends Component {
          <div className="row">
             <div className="eight columns">
 
-               <form action="" method="post" id="contactForm" name="contactForm">
-					<fieldset>
+               <form id="contactForm" name="contactForm" onChange={handleChange}>
+                  <fieldset>
 
-                  <div>
-						   <label htmlFor="contactName">Name <span className="required">*</span></label>
-						   <input type="text" defaultValue="" size="35" id="contactName" name="contactName" onChange={this.handleChange}/>
-                  </div>
+                     <div>
+                        <label htmlFor="contactName">Name <span className="required">*</span></label>
+                        <input value={name} type="text" size="35" id="contactName" name="contactName" onChange={e => setName(e.target.value)} />
+                     </div>
 
-                  <div>
-						   <label htmlFor="contactEmail">Email <span className="required">*</span></label>
-						   <input type="text" defaultValue="" size="35" id="contactEmail" name="contactEmail" onChange={this.handleChange}/>
-                  </div>
+                     <div>
+                        <label htmlFor="contactSubject">Subject</label>
+                        <input value={subject} type="text" size="35" id="contactSubject" name="contactSubject" onChange={e => setSubject(e.target.value)} />
+                     </div>
 
-                  <div>
-						   <label htmlFor="contactSubject">Subject</label>
-						   <input type="text" defaultValue="" size="35" id="contactSubject" name="contactSubject" onChange={this.handleChange}/>
-                  </div>
+                     <div>
+                        <label htmlFor="contactMessage">Message <span className="required">*</span></label>
+                        <textarea value={message} onChange={e => setMessage(e.target.value)} cols="50" rows="15" id="contactMessage" name="contactMessage"></textarea>
+                     </div>
 
-                  <div>
-                     <label htmlFor="contactMessage">Message <span className="required">*</span></label>
-                     <textarea cols="50" rows="15" id="contactMessage" name="contactMessage"></textarea>
-                  </div>
+                     <div>
+                        <button type='submit' onClick={handleClick} className="submit">Submit</button>
+                        <span id="image-loader">
+                           <img alt="" src="images/loader.gif" />
+                        </span>
+                     </div>
+                  </fieldset>
+               </form>
 
-                  <div>
-                     <button className="submit">Submit</button>
-                     <span id="image-loader">
-                        <img alt="" src="images/loader.gif" />
-                     </span>
-                  </div>
-					</fieldset>
-				   </form>
-
-           <div id="message-warning"> Error boy</div>
-				   <div id="message-success">
+               <div id="message-warning"> Error boy</div>
+               <div id="message-success">
                   <i className="fa fa-check"></i>Your message was sent, thank you!<br />
-				   </div>
-           </div>
+               </div>
+            </div>
 
 
             <aside className="four columns footer-widgets">
                <div className="widget widget_contact">
 
-					   <h4>Address and Phone</h4>
-					   <p className="address">
-						   {name}<br />
-						   {street} <br />
-						   {city}, {state} {zip}<br />
-						   <span>{phone}</span>
-					   </p>
-				   </div>
+                  <h4>Address and Phone</h4>
+                  <p className="address">
+                     {data?.name}<br />
+                     {data?.address.street} <br />
+                     {data?.address.city}, {data?.address.state} {data?.address.zip}<br />
+                     <span>{data?.phone}</span>
+                  </p>
+               </div>
 
                <div className="widget widget_tweets">
-                  <h4 className="widget-title">Latest Tweets</h4>
-                  <ul id="twitter">
-                     <li>
-                        <span>
-                        This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.
-                        Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum
-                        <a href="#">http://t.co/CGIrdxIlI3</a>
-                        </span>
-                        <b><a href="#">2 Days Ago</a></b>
-                     </li>
-                     <li>
-                        <span>
-                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
-                        eaque ipsa quae ab illo inventore veritatis et quasi
-                        <a href="#">http://t.co/CGIrdxIlI3</a>
-                        </span>
-                        <b><a href="#">3 Days Ago</a></b>
-                     </li>
-                  </ul>
-		         </div>
+
+               </div>
             </aside>
-      </div>
-   </section>
-    );
-  }
+         </div>
+      </section>
+   );
 }
 
 export default Contact;
