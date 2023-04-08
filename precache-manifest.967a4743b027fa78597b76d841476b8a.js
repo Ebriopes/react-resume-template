9595f173db4086e9f058615a227b633afa0ff167
ebriopes <antonio.vargasrosales@gmail.com>
2023-04-07 18:57:59 -0600

    Updates

---
+++ precache-manifest.967a4743b027fa78597b76d841476b8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e556091da3961555171161f2f4b710ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40603dee0811e86d2b67",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "e216cf17a61cbec47a11",
+    "url": "/static/js/2.cce2b280.chunk.js"
+  },
+  {
+    "revision": "52424457e5c4fa93c31028243ad4df2b",
+    "url": "/static/js/2.cce2b280.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40603dee0811e86d2b67",
+    "url": "/static/js/main.7b215ab1.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
